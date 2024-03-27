d84466a8b04ab255b360eb73d5c78d1d0c39f3da
Tarun <saikumarsathuluri0@gmail.com>
2024-03-27 20:50:14 +0530

    deleted

--- src/app/teams/teams-routing.module.ts
+++ src/app/teams/teams-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TeamComponent } from './team/team.component';
-import { PostsComponent } from './posts/posts.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'posts', pathMatch: 'full' },
-  { path: 'posts', component: PostsComponent },
-  { path: 'posts/:userId', component: PostsComponent },
   { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }
 ];
 
--- src/app/teams/teams.module.ts
+++ src/app/teams/teams.module.ts
@@ -4,15 +4,11 @@ import { CommonModule } from '@angular/common';
 import { TeamsRoutingModule } from './teams-routing.module';
 import { TeamsComponent } from './teams.component';
 import { TeamComponent } from './team/team.component';
-import { CommonService } from 'src/assets/services/common.service';
-import { PostsComponent } from './posts/posts.component';
-
 
 @NgModule({
   declarations: [
     TeamsComponent,
     TeamComponent,
-    PostsComponent
   ],
   imports: [
     CommonModule,
