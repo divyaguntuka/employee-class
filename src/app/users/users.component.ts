64ab6ffbfc276d352b257d0290e6b6f535f26948
divyaguntuka <divyaguntuka23@gmail.com>
2024-04-02 21:09:41 +0530

    add-edit-user

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -8,13 +8,18 @@ import { UserService } from './users.service';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+edit(_t10: User) {
+throw new Error('Method not implemented.');
+}
 
   public users: User[] = [];
+  selectedUser: any;
 
   constructor(public userService: UserService) { }
 
   ngOnInit(): void {
     this.getUsers();
+    this.close();
   }
 
   public getUsers() {
@@ -22,5 +27,24 @@ export class UsersComponent implements OnInit {
       this.users = res;
     });
   }
+
+    public saveOrEditUser(user: User) {
+    if (this.selectedUser.id) {
+      this.userService.updateUser(this.selectedUser.id, user).subscribe((res: any) => {
+        console.log('updated post : ', res);
+        this.close();
+      })
+    } else {
+      this.userService.saveUser(user).subscribe((res: any) => {
+        console.log('save post : ', res);
+        this.close();
+      })
+    }
+  }
+  close() {
+    throw new Error('Method not implemented.');
+  }
+
+
 
 }
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -2,12 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Posts } from 'src/assets/classes/posts';
+import { User } from 'src/assets/classes/User';
 
 @Injectable(
     { providedIn: 'root' }
 )
 
 export class UserService {
+    deleteUser(id: number) {
+      throw new Error('Method not implemented.');
+    }
+    // saveUser(user: User) {
+    //   throw new Error('Method not implemented.');
+    // }
     constructor(private http: HttpClient) { }
 
     private BASE_URL = 'https://jsonplaceholder.typicode.com/';
@@ -40,5 +47,17 @@ export class UserService {
     public deletePost(postId: number): Observable<any> {
         return this.http.delete(this.BASE_URL + `posts/${postId}`);
     }
+
+
+    //user
+
+    public saveUser(user: User): Observable<any> {
+        return this.http.post(this.BASE_URL + 'user', user);
+    }
+    public updateUser(userId: number, user: User): Observable<any> {
+        return this.http.put(this.BASE_URL + `user/${userId}`, user);
+    }
+
+
 
 }
